d06e99b8deeee63b0ca143f819793905b8147c9e
Tanay Shah <tanayssh@usc.edu>
2018-03-03 23:21:52 -0800

    Added the code for speech input

---
+++ speech/java/src/com/microsoft/cognitive_services/speech_recognition/examples/NBest.java
@@ -0,0 +1,9 @@
+package com.microsoft.cognitive_services.speech_recognition.examples;
+
+public class NBest {
+	private double Confidence;
+	private String Lexical;
+	private String ITN;
+	private String MaskedITN;
+	private String Display;
+}
---
+++ speech/java/src/com/microsoft/cognitive_services/speech_recognition/examples/speechInput.java
@@ -0,0 +1,10 @@
+package com.microsoft.cognitive_services.speech_recognition.examples;
+
+import java.util.List;
+
+public class speechInput {
+	private String RecognitionStatus;
+	private long Offset;
+	private long Duration;
+	private List<NBest> NBest;
+}
